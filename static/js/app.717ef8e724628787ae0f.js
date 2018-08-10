ba5584cde409c7d10bb4c1f9a44358119b832c13
TakHyun-Kang <azx1054@gmail.com>
2018-08-10 16:53:30 +0900

    Updates

---
+++ static/js/app.717ef8e724628787ae0f.js
@@ -0,0 +1,2 @@
+webpackJsonp([0],[function(n,e,o){"use strict";Object.defineProperty(e,"__esModule",{value:!0});var t=o(1),a=(o.n(t),o(3)),c=(o.n(a),o(4));o.n(c)},function(n,e){},,function(n,e){},function(n,e){var o=document.getElementById("colorTab"),t=document.getElementById("fontTab"),a=function(){"/colorpage.html"===window.location.pathname?o.className+="active":"/fontpage.html"===window.location.pathname&&(t.className+="active")};window.onload=a}],[0]);
+//# sourceMappingURL=app.717ef8e724628787ae0f.js.map
