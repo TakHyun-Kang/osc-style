33a7405b4f7271280fe44e7873509544a7f975ff
TakHyun-Kang <azx1054@gmail.com>
2018-08-13 13:16:35 +0900

    no message

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,10 +1,10 @@
 const _colorTab = document.getElementById('colorTab')
-const colorPath = '/osc-style/colorpage.html'
+const colorPath = 'colorpage'
 const _fontTab = document.getElementById('fontTab')
 const fontPath = '/osc-style/fontpage.html'
 
 const _handleClick = () => {
-  if (window.location.pathname === colorPath) {
+  if (window.location.pathname.indexOf(colorPath) > -1) {
     _colorTab.className += 'active'
   } else if (window.location.pathname === fontPath) {
     _fontTab.className += 'active'
